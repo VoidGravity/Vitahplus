257d2142c80dfbf0c0ef17de953ec238bfa215c8
VoidGravity <abdellahbardichwork@gmail.com>
2024-04-27 16:42:51 +0100

    Email view :fire:

--- VHplus/app/Mail/MailNotify.php
+++ VHplus/app/Mail/MailNotify.php
@@ -38,7 +38,8 @@ class MailNotify extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'email.appointment',
+            with: ['status' => $this->status]
         );
     }
 
